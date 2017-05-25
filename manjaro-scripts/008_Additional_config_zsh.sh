dd6844be57ad340e4bc849bdd7628cc83b6402a9
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2017-05-25 16:18:32 +0300

    Updated scripts

---
+++ manjaro-scripts/008_Additional_config_zsh.sh
@@ -0,0 +1,4 @@
+yaourt -S zsh-autosuggestions --noconfirm
+echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+yaourt -S zsh-syntax-highlighting --noconfirm
+echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
