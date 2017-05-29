f9ac881f62f7a4b314e7108a1d4a7512e0de72b6
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2017-05-29 14:18:57 +0300

    Updated 008_Additional_config_zsh.sh

--- manjaro-scripts/008_Additional_config_zsh.sh
+++ manjaro-scripts/008_Additional_config_zsh.sh
@@ -2,3 +2,5 @@ yaourt -S zsh-autosuggestions --noconfirm
 echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 yaourt -S zsh-syntax-highlighting --noconfirm
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "export EDITOR=vim
+export VISUAL=vim" >> ~/.zshrc
